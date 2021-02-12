bda6343eb08293e656877fb714617aead6bc1523
kubilaycicek <kubilaycicek0693@gmail.com>
2021-02-12 11:37:06 +0300

    Assertions On Collections

--- assertj-tutorial/src/test/java/StudentTestWithAsserJAssertions.java
+++ assertj-tutorial/src/test/java/StudentTestWithAsserJAssertions.java
@@ -1,5 +1,6 @@
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Condition;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -7,6 +8,8 @@ import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.assertj.core.api.filter.InFilter.in;
 
 public class StudentTestWithAsserJAssertions {
 
@@ -37,14 +40,14 @@ public class StudentTestWithAsserJAssertions {
 
     @Test
     void studentList() {
+
         final Student student1 = new Student("Jhon", "Doe", LocalDate.of(1990, Month.SEPTEMBER, 5));
         final Student student2 = new Student("Mark", "Walker", LocalDate.of(1969, Month.AUGUST, 12));
         final Student student3 = new Student("Michael", "Jackson", LocalDate.of(2000, Month.JANUARY, 16));
         final Student student4 = new Student("Dominic", "Toretto", LocalDate.of(2005, Month.DECEMBER, 7));
-        final Student student5 = new Student("Kubilay", "Cicek", LocalDate.of(1993, Month.MAY, 13));
+        final Student student5 = new Student("Kubilay", "Cicek", LocalDate.of(1990, Month.MAY, 13));
 
-
-        List<Student> students = Arrays.asList(student1, student2, student3, student4);
+        final List<Student> students = Arrays.asList(student1, student2, student3, student4);
 
         Assertions.assertThat(students).as("Student's List")
                 .isNotNull()
@@ -58,17 +61,33 @@ public class StudentTestWithAsserJAssertions {
 
         Assertions.assertThat(students)
                 .filteredOn(student -> student.getBirthDate().until(LocalDate.now(), ChronoUnit.YEARS) >= 25)
-                .hasSize(3)
-                .containsOnly(student5, student2, student1);
+                .contains(student1, student2);
 
         Assertions.assertThat(students).filteredOn(new Condition<Student>() {
             @Override
             public boolean matches(Student student) {
                 return student.getBirthDate().until(LocalDate.now(), ChronoUnit.YEARS) >= 25;
             }
-        }).hasSize(3)
-                .containsOnly(student5, student2, student1);
+        }).hasSize(2)
+                .containsOnly(student1, student2);
 
 
+        Assertions.assertThat(students)
+                .filteredOn("birthDate",in(LocalDate.of(1990, Month.SEPTEMBER, 5))).hasSize(1)
+                .containsOnly(student1);
+
+        Assertions.assertThat(students)
+                .extracting(Student::getName)
+                .filteredOn(name->name.contains("a"))
+                .hasSize(2)
+                .containsOnly("Mark","Michael");
+
+        Assertions.assertThat(students)
+                .filteredOn(student -> student.getName().contains("a"))
+                .extracting(Student::getName,Student::getSurname)
+                .containsOnly(
+                        Tuple.tuple("Mark","Walker"),
+                        Tuple.tuple("Michael","Jackson")
+                );
     }
 }
